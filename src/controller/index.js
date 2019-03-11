1482ca8fb1d7bd8156fa8232a3555d41c4560467
JanetGM <antaly.146@gmail.com>
2019-03-11 18:45:41 -0500

    modificando el package.jason e index.js

--- src/controller/index.js
+++ src/controller/index.js
@@ -8,8 +8,10 @@ export const mdLinks = (root, options) => {
   // cuando los dos son falsos
   if (options.validate) {
     return validateLinks(root);
-  } else if (!options.validate){
-     return getPropertiesOfDocumentMd(root);
+  } else if (!options.validate) {
+    return new Promise((resolve) => {
+      resolve(getPropertiesOfDocumentMd(root));
+    });
   }
 };
 // mdLinks('C:\\Users\\Usuario\\Documents\\ProjectsLaboratoria\\LIM008-fe-md-links\\test\\testFolder\\folder1\\folder1a', options).then(resp => console.log(resp))
