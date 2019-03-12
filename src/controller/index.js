064bd91c4dbc9806b118d7f1a4a963de2b1b5979
JanetGM <antaly.146@gmail.com>
2019-03-11 20:48:32 -0500

    modificando la funcion md-links para que cuando reciba un parametro retorne el objeto

--- src/controller/index.js
+++ src/controller/index.js
@@ -3,15 +3,22 @@ import {validateLinks, statLinks, statLinksBroken} from '../controller/options.j
 
 const options = {
   validate: true
-}
+};
 export const mdLinks = (root, options) => {
+  if (!options) {
+    return new Promise((resolve, reject) => {
+      resolve(getPropertiesOfDocumentMd(root));
+    });
+  }
   // cuando los dos son falsos
   if (options.validate) {
     return validateLinks(root);
-  } else if (!options.validate) {
-    return new Promise((resolve) => {
+  } 
+  if (options.validate === false) {
+    return new Promise((resolve, reject) => {
       resolve(getPropertiesOfDocumentMd(root));
     });
   }
 };
-// mdLinks('C:\\Users\\Usuario\\Documents\\ProjectsLaboratoria\\LIM008-fe-md-links\\test\\testFolder\\folder1\\folder1a', {validate: false }).then(resp => console.log(resp))
+mdLinks('C:\\Users\\Usuario\\Documents\\ProjectsLaboratoria\\LIM008-fe-md-links\\test\\testFolder\\folder1\\folder1a')
+  .then(resp => console.log(resp));
