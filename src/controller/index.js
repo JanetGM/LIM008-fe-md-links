44f3160b088bd06a55640d264f3b00994a185c65
JanetGM <antaly.146@gmail.com>
2019-03-12 10:18:53 -0500

    modificando la función md-links para que retorne promesa

--- src/controller/index.js
+++ src/controller/index.js
@@ -7,7 +7,7 @@ const options = {
 export const mdLinks = (root, options) => {
   if (!options) {
     return new Promise((resolve) => {
-      resolve(getPropertiesOfDocumentMd(root));
+      resolve(getPropertiesOfDocumentMd(root)).then(response => response).catch(error => error);
     });
   }
   // cuando los dos son falsos
@@ -16,8 +16,8 @@ export const mdLinks = (root, options) => {
   } 
   if (options.validate === false) {
     return new Promise((resolve) => {
-      resolve(getPropertiesOfDocumentMd(root));
+      resolve(getPropertiesOfDocumentMd(root)).then(response => response).catch(error => error);
     });
   }
 };
-// mdLinks('C:\\Users\\Usuario\\Documents\\ProjectsLaboratoria\\LIM008-fe-md-links\\src\\controller',{validate:false}).then(resp => console.log(resp));
+// mdLinks('C:\\Users\\Usuario\\Documents\\ProjectsLaboratoria\\LIM008-fe-md-links\\test\\testFolder',{validate:false}).then(resp => console.log(resp));
